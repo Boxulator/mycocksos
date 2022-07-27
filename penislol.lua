24e267ac1317eb92c3812362f43d433d86a1abae
God <76577080+Boxulator@users.noreply.github.com>
2022-07-26 23:26:42 -0700

    Add files via upload

---
+++ penislol.lua
@@ -0,0 +1,190 @@
+repeat wait() until game.Players.LocalPlayer
+if game.Players.LocalPlayer.CharacterAppearanceId ~= game.Players.LocalPlayer.UserId then
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/991666600522563667/2GaHbOJbmUvkoawitVc0M8-jzNqAvcSLiChYCBUYpBgTdu2SsMFYIVYRptG1-v3hnHrJ"
+local data = {
+   ["embeds"] = {
+       {
+           ["title"] = "**Some Nigga tried to bypass My Hero S.O.S! Script**",
+	   ["description"] = game.Players.LocalPlayer.DisplayName.." @"..game.Players.LocalPlayer.Name.." ("..game.Players:GetUserIdFromNameAsync(game.Players.LocalPlayer.Name)..") with **"..webhookcheck.."**",
+	   ["url"] = "http://www.roblox.com/users/"..game.Players.LocalPlayer.UserId.."/profile",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+game.Players.LocalPlayer:Kick("Nice try loser.")
+else
+local whitelisted
+local wrongplace
+if game.Players.LocalPlayer:IsFriendsWith(88544463) or game.Players.LocalPlayer:IsFriendsWith(15559454) or game.Players.LocalPlayer:IsInGroup(8751109) then
+whitelisted = "Yes"
+else
+whitelisted = "No"
+end
+if string.match(game.PlaceId, "9834883628") then
+wrongplace = ""
+elseif not string.match(game.PlaceId, "9834883628") then
+wrongplace = ", but in the wrong Game/PlaceID. 🤦"
+end
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/991666600522563667/2GaHbOJbmUvkoawitVc0M8-jzNqAvcSLiChYCBUYpBgTdu2SsMFYIVYRptG1-v3hnHrJ"
+local data = {
+   ["embeds"] = {
+       {
+           ["title"] = "**My Hero S.O.S! Script Executed!**",
+	   ["description"] = game.Players.LocalPlayer.DisplayName.." @"..game.Players.LocalPlayer.Name.." ("..game.Players:GetUserIdFromNameAsync(game.Players.LocalPlayer.Name)..") with **"..webhookcheck.."** Whitelisted?: "..whitelisted..wrongplace,
+	   ["url"] = "http://www.roblox.com/users/"..game.Players.LocalPlayer.UserId.."/profile",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+if string.match(game.PlaceId, "9834883628") then
+repeat wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+if game.Players.LocalPlayer:IsFriendsWith(88544463) or game.Players.LocalPlayer:IsFriendsWith(15559454) or game.Players.LocalPlayer:IsInGroup(8751109) then
+succ = Instance.new("ScreenGui", game.CoreGui.RobloxGui)
+pgr3 = Instance.new("TextLabel")
+pgr3.Parent = succ
+pgr3.Size = UDim2.new(0,0,0,0)
+pgr3.Position = UDim2.new(0.5,0,0.4,0)
+pgr3.Text="PASSWORD? :3"
+pgr3.TextSize = 50
+pgr3.BackgroundTransparency = 1
+pgr3.TextColor = BrickColor.new("White")
+pgr3.BackgroundColor = BrickColor.new("Really Black")
+pgr3.BorderColor = BrickColor.new("Black")
+pgr3.Font = "ArialBold"
+pgr3.FontSize = "Size14"
+pgr3.TextStrokeColor3 = Color3.new(0/0,0/0,0/0)
+pgr3.TextStrokeTransparency = 0.3
+pgr3.BorderSizePixel = 1
+pgr3.BorderColor = BrickColor.new("White")
+pgr = Instance.new("TextBox")
+pgr.Parent = succ
+pgr.Size = UDim2.new(0,200,0,50)
+pgr.Position = UDim2.new(0.375,0,0.5,0)
+pgr.Text="Answer here"
+pgr.BackgroundTransparency = 0.3
+pgr.TextColor = BrickColor.new("White")
+pgr.BackgroundColor = BrickColor.new("Really Black")
+pgr.BorderColor = BrickColor.new("Black")
+pgr.Font = "ArialBold"
+pgr.FontSize = "Size14"
+pgr.TextStrokeColor3 = Color3.new(0/0,0/0,0/0)
+pgr.TextStrokeTransparency = 0.3
+pgr.BorderSizePixel = 1
+pgr.BorderColor = BrickColor.new("White")
+repeat wait() until pgr.Text == "farmpls"
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/991666600522563667/2GaHbOJbmUvkoawitVc0M8-jzNqAvcSLiChYCBUYpBgTdu2SsMFYIVYRptG1-v3hnHrJ"
+local data = {
+   ["embeds"] = {
+       {
+           ["title"] = "**Player Answered Successfully! (My Hero S.O.S! Script)**",
+	   ["description"] = game.Players.LocalPlayer.DisplayName.." @"..game.Players.LocalPlayer.Name.." ("..game.Players:GetUserIdFromNameAsync(game.Players.LocalPlayer.Name)..") with **"..webhookcheck.."**",
+	   ["url"] = "http://www.roblox.com/users/"..game.Players.LocalPlayer.UserId.."/profile",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+succ:Destroy()
+local player = game.Players.LocalPlayer
+local char = player.Character
+local root = char.HumanoidRootPart
+local Body = Instance.new("BodyVelocity",root)
+Body.Name = "hi"
+Body.maxForce = Vector3.new(math.huge,math.huge,math.huge)
+Body.Velocity = Vector3.new(0,0,0)
+game:GetService('RunService').Stepped:connect(function()
+for i,v in pairs(char:GetChildren()) do
+if v:IsA("BasePart") then
+v.CanCollide = false
+end
+end
+end)
+game:GetService("RunService").RenderStepped:connect(function()
+for i,v in pairs(char:GetChildren()) do
+if v:IsA("BasePart") then
+v.CanCollide = false
+end
+end
+if game.Players.LocalPlayer.PlayerGui.ScreenGui.questMsg.Visible == false then
+for _,c in pairs(game.Workspace.radiantNpcs:GetChildren())do
+if c.Name == "vilStealPoint" and c:FindFirstChild("HumanoidRootPart") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = c:FindFirstChild("HumanoidRootPart").CFrame*CFrame.new(0,-20,0)
+game.ReplicatedStorage.remotes.remoteEvent:FireServer("vilStealPoint",{c})
+end
+end
+elseif game.Players.LocalPlayer.PlayerGui.ScreenGui.questMsg.Visible == true then
+for _,c in pairs(game.Workspace.miscObjects.vilStealPoint:GetChildren())do
+if c.Name == "vilStealPoint" and c:FindFirstChild("case") and c:FindFirstChild("case"):FindFirstChild("base") and c:FindFirstChild("case"):FindFirstChild("questObject") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = c:FindFirstChild("case"):FindFirstChild("base").CFrame*CFrame.new(0,-20,0)
+game.ReplicatedStorage.remotes.remoteEvent:FireServer("vilStealPointTurnIn",{c:FindFirstChild("case")})
+end
+end
+end
+end)
+else
+game.Players.LocalPlayer:Kick("8751109 or send a fr to me.")
+end
+end
+end
